3cee39013c98369e047ba73ae4021b20f75dc3b0
meteor <tjdans34568@gmail.com>
2023-01-10 17:49:53 +0900

    pop method practice

--- lib/screen/route/route_two_screen.dart
+++ lib/screen/route/route_two_screen.dart
@@ -1,4 +1,6 @@
 import 'package:button_practice/layout/main_layout.dart';
+import 'package:button_practice/screen/route/route_three_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RouteTwoScreen extends StatelessWidget {
@@ -22,7 +24,31 @@ class RouteTwoScreen extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).pushNamed("/three", arguments: 999);
           },
-          child: Text("push named"))
+          child: Text("push named")),
+      ElevatedButton(
+          onPressed: () {
+            // pushReplacement 는 이전 페이지를 지우고 페이지가 이동된다.
+            // 페이지를 대체하고 스택에 쌓이는 느낌
+            Navigator.of(context).pushReplacement(
+                CupertinoPageRoute(builder: (_) => RouteThreeScreen()));
+          },
+          child: Text("Push Replacement")),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (_) => RouteThreeScreen()),
+              (route) => route.settings.name == "/",
+            );
+          },
+          child: Text("push And Remove Until")),
+      ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              "/three",
+              (route) => route.settings.name == "/",
+            );
+          },
+          child: Text("push Named And Remove Until")),
     ]);
   }
 }
